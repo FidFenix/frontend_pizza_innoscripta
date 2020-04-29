4e32439a6f13f715a45e8c68bc7d0b2d78a89a43
FidFenix <fid.mamani@gmail.com>
2020-04-29 14:45:02 -0500

    testing functionality for adding items to cart or retire them from cart

--- src/test/reducer.cart..test.js
+++ src/test/reducer.cart..test.js
@@ -11,3 +11,29 @@ describe('Adding zero items to cart, it should return the prev state of cart', (
                           )).toEqual({ hidden: true, cartItems: [] });
     })
 })
+
+describe('Removing items from cart', ()=> {
+  it('should return the initial state', () => {
+      expect(cartReducer( { hidden: true, cartItems: [{'id': 3,
+        'imageUrl': "https://cdn.pixabay.com/photo/2019/08/23/13/05/pizza-4425691_960_720.jpg",
+        'name': "Big Pizza Dough Cheese",
+        'price': 35,
+        'quantity': 2}] }, 
+                          {action: { 'type': CartActionTypes.REMOVE_ITEM,
+                                     'payload': {
+                                      id: 3,
+                                      imageUrl: "https://cdn.pixabay.com/photo/2019/08/23/13/05/pizza-4425691_960_720.jpg",
+                                      name: "Big Pizza Dough Cheese",
+                                      price: 35,
+                                      quantity: 1
+                                     }
+                                   }
+                          } 
+                        )).toEqual({ hidden: true, cartItems: [{'id': 3,
+                        'imageUrl': "https://cdn.pixabay.com/photo/2019/08/23/13/05/pizza-4425691_960_720.jpg",
+                        'name': "Big Pizza Dough Cheese",
+                        'price': 35,
+                        'quantity': 2}] });
+  })
+})
+
