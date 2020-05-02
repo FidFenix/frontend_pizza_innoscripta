b2c5f447a1d8fc86e28c59e8e096343bad192c6a
FidFenix <fid.mamani@gmail.com>
2020-05-01 19:51:28 -0500

    Deleting fake backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/home-page/home-page-component';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
