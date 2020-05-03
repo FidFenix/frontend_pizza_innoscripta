18a8d81f825ab6a6b80aeb2ae6f240075f96d361
FidFenix <fid.mamani@gmail.com>
2020-05-02 20:16:12 -0500

    merging cnfig files

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
 const config = { apiUrl: 'http://ec2-18-156-176-169.eu-central-1.compute.amazonaws.com'};
 export default config;
-=======
-const config = { apiUrl: 'http://localhost'};
-export default config;
-<<<<<<< HEAD
->>>>>>> development
-=======
->>>>>>> development
